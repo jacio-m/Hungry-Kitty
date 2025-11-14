f45527f29cac79320b037a4f042d935d18693fdf
Jácio Mauê <jaciomaue@gmail.com>
2025-11-14 19:01:15 -0300

    Added Player Sprites and Enhanced Movement
    Added player sprites (popcat) and made some changes to the player script so the sprites can work.

--- hungry-kitty/Player/player.gd
+++ hungry-kitty/Player/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
-var speed = 250
+var speed = 350
 
-func _process(delta):
+func _physics_process(delta):
+	$AnimatedSprite2D.play("Idle")
 	if Input.is_action_just_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = true
 		velocity.x = speed * -1
 	if Input.is_action_just_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = false
 		velocity.x = speed
-	if Input.is_action_just_released("ui_left"):
+	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
 		velocity.x = 0
-	if Input.is_action_just_released("ui_right"):
-		velocity.x = 0	
 	move_and_slide()
